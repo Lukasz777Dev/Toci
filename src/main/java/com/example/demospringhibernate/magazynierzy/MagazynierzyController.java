5efd4a22ee8b6bcbd4e5e6bd8b70e3d80be2bb1b
Łukasz Kędzierski <11044@pwsz.wloclawek.pl>
2021-07-04 22:14:11 +0200

    Add HTML file which will be contain a form - Magazynierzy.

--- src/main/java/com/example/demospringhibernate/magazynierzy/MagazynierzyController.java
+++ src/main/java/com/example/demospringhibernate/magazynierzy/MagazynierzyController.java
@@ -3,25 +3,31 @@ package com.example.demospringhibernate.magazynierzy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController //uzywam protokolu HTTP
-// @RequestMapping(path = "api/magazynierzy") //na cala klase ustawilem, ze
+@RequestMapping(path = "api/magazynierzy") //na cala klase ustawilem, ze
 
 public class MagazynierzyController {
 
     private final MagazynierzyService magazynierzyService;
 
     @RequestMapping(value = "/magazynier", method = RequestMethod.GET)
-    public ModelAndView showFormMain{
+    public ModelAndView showFormMain(){
         return new ModelAndView("magazynierzyView", "magazynierzy", new Magazynierzy());
     }
 
     @RequestMapping(value = "/addMagazynier", method = RequestMethod.POST)
-    public String submit(@Valid @ModelAttribute("magazynierzy"), Magazynierzy magazynierzy, BindingResult result, ModelMap modelMap)
+    public String submit(/*@Valid*/ @ModelAttribute("magazynierzy"), Magazynierzy magazynierzy, BindingResult result, ModelMap modelMap){
+       modelMap.addAttribute("name", magazynierzy.getMagazynierzyNazwisko());
+       modelMap.addAttribute("number",magazynierzy.getMagazynierzyNr());
+
+        return "magazynierzyView";
+    }
 
     @Autowired
       public MagazynierzyController(MagazynierzyService magazynierzyService) {
